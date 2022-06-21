5d78c4628843ecdb194af2e532b262b1a344ea51
Mohit Chandel <devmohit93@gmail.com>
2022-06-21 14:55:36 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(UserRoute);
 app.use(NftRoute);
 app.use(CollectionRoute);
 
-const PORT = process.env.PORT || 5100
+const PORT = process.env.PORT || 5000
 
 mongoose
     .connect("mongodb+srv://isha:isha2180@cluster0.govgi.mongodb.net/?retryWrites=true&w=majority", {
