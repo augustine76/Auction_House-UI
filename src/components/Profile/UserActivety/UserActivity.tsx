4207f5f2973d878a587e3df5fb075a2c1d2bc951
IshaMandaviya <isha@solulab.com>
2022-07-01 15:54:03 +0530

    user activity done

--- src/components/Profile/UserActivety/UserActivity.tsx
+++ src/components/Profile/UserActivety/UserActivity.tsx
@@ -1,23 +1,45 @@
 import { Table, useAsyncList } from "@nextui-org/react";
-
+import axios from "axios";
+import { useWallet } from "@solana/wallet-adapter-react";
+const baseURL = "http://localhost:5100";
 export default function UserActivity() {
   const columns = [
-    { name: "Name", uid: "name" },
-    { name: "Height", uid: "height" },
-    { name: "Mass", uid: "mass" },
-    { name: "Birth Year", uid: "birth_year" },
+    { name: "MintKey", uid: "mintKey" },
+    { name: "Type", uid: "type" },
+    { name: "Buyer", uid: "buyer" },
+    { name: "Seller", uid: "seller" },
+    { name: "PriceAmount", uid: "priceAmount" },
+    { name: "TransctionID", uid: "transactionId" },
+    { name: "Timestemp", uid: "timeStamp" },
   ];
+  const { publicKey } = useWallet();
   async function load({ signal, cursor }) {
     // If no cursor is available, then we're loading the first page.
     // Otherwise, the cursor is the next URL to load, as returned from the previous page.
+
+
+   const res2= await axios
+        .get(`${baseURL}/getUserDetails/${publicKey}`)
+        .then((response) => {
+          console.log("user details", response);
+          // console.log("pub key", response.data.data.publicKey);
+          return response.data.data.activity;
+        })
+        .catch((error) => {
+          console.error("There was an error!", error);
+        });
+        console.log(res2)
+
+
     const res = await fetch(
       cursor || "https://swapi.py4e.com/api/people/?search=",
       { signal }
     );
     const json = await res.json();
+    console.log("json",json);
     return {
-      items: json.results,
-      cursor: json.next,
+      items: res2,
+      // cursor: json.next,
     };
   }
   const list = useAsyncList({ load });
@@ -41,7 +63,7 @@ export default function UserActivity() {
         onLoadMore={list.loadMore}
       >
         {(item) => (
-          <Table.Row key={item.name}>
+          <Table.Row key={item}>
             {(key) => (
               <Table.Cell css={{ color: "#fff" }}>{item[key]}</Table.Cell>
             )}
